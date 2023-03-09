63c55967810e020770b7b2ce20b726038e8d258b
Gabriel <gabrigomez15@gmail.com>
2023-03-09 09:52:36 -0300

    fix: wip vercel troubleshooting

--- api/index.js
+++ api/index.js
@@ -17,7 +17,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get('/api', (req, res) => {
+app.get('/', (req, res) => {
   res.status(200).json({ msg: "Welcome to the my-game-list api"});
 });
 
